93fc4d55f69a0d96b35efa1c109c97e2f94e304a
Lloyd Tanedo <hackintosh@Lloyds-MacBook-Pro.local>
2022-02-11 11:09:21 -0500

    configure setup files for pypi

---
+++ README.md
@@ -0,0 +1,2 @@
+# Easy Environment
+A python library to rapidly set, get, and view local environment variables (currently only for bash and zsh shells in unix).
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'lloydtan',                   # Type in your name
   author_email = 'lloydtan@buffalo.edu',      # Type in your E-Mail
   url = 'https://github.com/ltanedo/easy-environ.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ltanedo/easy-environ/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['python', 'env', 'environment'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'glob',
